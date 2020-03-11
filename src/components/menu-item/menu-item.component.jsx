6850fcbc8da30019593529e91c0979ece5dd3d4c
Subramaniam <subramaniam.vls@gmail.com>
2020-03-11 14:52:49 +0530

    11-MAR-20

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -8,6 +8,12 @@ const MenuItem = ({ title, imageUrl, size }) => (
     }}
     className={`${size} menu-item`}
   >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    ></div>
     <div className="content">
       <h1 className="title"> {title.toUpperCase()} </h1>
       <span className="subtitle"> SHOP NOW</span>
